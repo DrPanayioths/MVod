2b7b450046fafa4a8f81c0eaff8223ef98fc33a1
DrPanayioths <panayioths0072019@gmail.com>
2024-10-28 01:14:21 +0200

    Base

---
+++ renderer.js
@@ -0,0 +1,20 @@
+const fileInput = document.getElementById('fileInput');
+const audioPlayer = document.getElementById('audioPlayer');
+const videoPlayer = document.getElementById('videoPlayer');
+
+fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    const fileURL = URL.createObjectURL(file);
+
+    if (file.type.startsWith('audio/')) {
+        audioPlayer.src = fileURL;
+        audioPlayer.play();
+        videoPlayer.style.display = 'none';
+        audioPlayer.style.display = 'block';
+    } else if (file.type.startsWith('video/')) {
+        videoPlayer.src = fileURL;
+        videoPlayer.play();
+        audioPlayer.style.display = 'none';
+        videoPlayer.style.display = 'block';
+    }
+});
