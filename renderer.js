e179afa3747da3af44a41d64bfc15c2b25ac906f
DrPanayioths <panayioths0072019@gmail.com>
2024-10-30 22:21:18 +0200

    Fix Cursor And Play/Pause Function And Pause Icon
    Fix Cursor Not Working Correctly And Made Play And Stop Functions Work Also Pause Button Image Was Added

--- renderer.js
+++ renderer.js
@@ -24,3 +24,4 @@ document.addEventListener('mousemove', (e) => {
     document.getElementById('cursor').style.left = `${e.pageX}px`
     document.getElementById('cursor').style.top = `${e.pageY}px`
 })
+
