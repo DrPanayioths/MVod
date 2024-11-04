3bd7a4869eebaacdabd81672be0efb1ef9ee8648
DrPanayioths <panayioths0072019@gmail.com>
2024-11-04 19:32:22 +0200

    UI Layout Change And Better Code Typo

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 500,
+        autoHideMenuBar: true,
+        // frame: false,
         webPreferences: {
             preload: path.join(__dirname, 'renderer.js'),
             contextIsolation: true,
@@ -18,21 +20,3 @@ function createWindow() {
 app.whenReady().then(createWindow);
 
 
-// Progress Bar 
-const progressBar = document.getElementById('progress-bar');
-const progressBarContainer = document.getElementById('progress-bar-container');
-
-videoPlayer.addEventListener('timeupdate', () => {
-    const progress = (videoPlayer.currentTime / videoPlayer.duration) * 100;
-    progressBar.style.width = progress + '%';
-});
-
-progressBarContainer.addEventListener('click', (e) => {
-    const rect = progressBarContainer.getBoundingClientRect();
-    const pos = (e.clientX - rect.left) / rect.width;
-    videoPlayer.currentTime = pos * videoPlayer.duration;
-    audioPlayer.currentTime = pos * videoPlayer.duration;
-});
-
-
-// Volume Control
--- renderer.js
+++ renderer.js
@@ -19,13 +19,27 @@ fileInput.addEventListener('change', (event) => {
     }
 });
 
+// Progress Bar 
+document.addEventListener('DOMContentLoaded', () => {
+    const progress_bar = document.getElementById('progressbar_progress');
+    const progress_frame = document.getElementById('progressbar_main');
+    const Video_Player = document.getElementById('videoPlayer');
+    const Audio_Player = document.getElementById('audioPlayer');
 
+    Video_Player.addEventListener('timeupdate', () => {
+        const progress = (Video_Player.currentTime / Video_Player.duration) * 100;
+        progress_bar.style.width = progress + '%';
+    });
+    progress_frame.addEventListener('click', (e) => {
+        const rect = progress_frame.getBoundingClientRect();
+        const pos = (e.clientX - rect.left) / rect.width;
+        Video_Player.currentTime = pos * Video_Player.duration;
+        Audio_Player.currentTime = pos * Audio_Player.duration;
+    });
+});
 
 // Custom Cursor 
 document.addEventListener('mousemove', (e) => {
     document.getElementById('cursor').style.left = `${e.pageX}px`
     document.getElementById('cursor').style.top = `${e.pageY}px`
 });
-
-
-
